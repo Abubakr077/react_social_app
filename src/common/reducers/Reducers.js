f2776c1fcbb9948a01c801abf539a09e1de8da12
Muhammad Abubakar <bakrafzal0332@gmail.com>
2019-07-19 10:53:55 +0500

    redux update

---
+++ src/common/reducers/Reducers.js
@@ -0,0 +1,11 @@
+const AppReducer = (state = [], action) => {
+    switch(action.type) {
+        case 'UPDATE_NOTE_BOOKS':
+            // return state.concat([action.data]);
+            return action.data;
+
+        default:
+            return state;
+    }
+}
+export default AppReducer;
