3ff2024267a056bc26f3a1f7bbc80abc2eeeb996
Muhammad Abubakar <bakrafzal0332@gmail.com>
2019-07-19 11:45:31 +0500

    redux reducer update

--- src/index.js
+++ src/index.js
@@ -7,6 +7,17 @@ import * as serviceWorker from './common/serviceWorker';
 // App
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//redux
+import Reducers  from "./common/reducers/index";
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+
+
+const store = createStore(Reducers);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, document.getElementById('root'));
 
 serviceWorker.unregister();
